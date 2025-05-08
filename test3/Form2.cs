d9908388acdcdb5126f19bd9578e91d8984cd1f5
pomaraczus <buhahahahah052gmail.com>
2025-05-08 10:19:15 +0200

    popraw JSON

--- test3/Form2.cs
+++ test3/Form2.cs
@@ -193,7 +193,7 @@ namespace test3
         private void JSONzap_Click(object sender, EventArgs e)
         {
             SaveFileDialog saveFileDialog1 = new SaveFileDialog();
-            saveFileDialog1.Filter = "Pliki CSV (*)|*|Wszystkie pliki (*.*)|*.*";
+            saveFileDialog1.Filter = "Pliki JSON (*)|*|Wszystkie pliki (*.*)|*.*";
             saveFileDialog1.Title = "Wybierz lokalizację zapisu pliku JSON";
             saveFileDialog1.ShowDialog();
             // Jeśli użytkownik wybierze lokalizację i zatwierdzi, zapisz plik CSV
@@ -240,8 +240,8 @@ namespace test3
         {
             // Wyświetlenie okna dialogowego wyboru pliku CSV
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
-            openFileDialog1.Filter = "Pliki CSV (*.csv)|*.csv|Wszystkie pliki (*.*)|*.*";
-            openFileDialog1.Title = "Wybierz plik CSV do wczytania";
+            openFileDialog1.Filter = "Pliki JSON (*.JSON)|*.csv|Wszystkie pliki (*.*)|*.*";
+            openFileDialog1.Title = "Wybierz plik JSON do wczytania";
             openFileDialog1.ShowDialog();
             // Jeśli użytkownik wybierze plik i zatwierdzi, wczytaj dane z pliku CSV
             if (openFileDialog1.FileName != "")
